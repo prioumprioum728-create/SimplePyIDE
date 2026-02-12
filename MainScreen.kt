d41003f23a4937b75d9064f5860edce33a439497
prioumprioum728-create <prioumprioum728@gmail.com>
2026-02-12 15:27:02 +0600

    Create MainScreen.kt

---
+++ MainScreen.kt
@@ -0,0 +1,8 @@
+package com.example.simplepyide
+
+// This is your main Kotlin activity file
+// Handles editor and terminal setup
+
+class MainScreen {
+    // Your code here for code editor and terminal
+}
